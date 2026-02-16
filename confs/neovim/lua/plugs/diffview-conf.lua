8a2c476d580a634b27a5433ce807e38be930e66e
tanphat199@gmail.com <tanphat199@gmail.com>
2026-02-16 09:42:57 +0700

    enable diffview + gitsign

---
+++ confs/neovim/lua/plugs/diffview-conf.lua
@@ -0,0 +1,11 @@
+local mod = {
+	"sindrets/diffview.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	opts = {
+		use_icons = true,
+	},
+}
+
+return mod
---
+++ confs/neovim/lua/plugs/gitsigns-conf.lua
@@ -0,0 +1,8 @@
+local mod = {
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		current_line_blame = false,
+	},
+}
+
+return mod
